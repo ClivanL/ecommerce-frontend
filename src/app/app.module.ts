46b547550e51dbbb6a4c0057f06941d5afef8510
Clivan Lee <clivan93@gmail.com>
2022-11-11 16:27:32 +0800

    added pre-configured existing entities available in backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { UserComponent } from './user/user.component';
+import { PurchaseLogComponent } from './purchase-log/purchase-log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    UserComponent,
+    PurchaseLogComponent
   ],
   imports: [
     BrowserModule,
